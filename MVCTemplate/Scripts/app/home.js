b25108ffa13ae2556fd5599160060663c864976f
Oscar Alfaro <16330497@itnogales.edu.mx>
2022-09-02 14:07:16 -0700

    Avance

--- MVCTemplate/Scripts/app/home.js
+++ MVCTemplate/Scripts/app/home.js
@@ -3,3 +3,18 @@ $(document).ready(function () {
     $('#spanHome').css({ 'background-color': '#ffa500', 'color': '#fff' });
 
 });
+const video = document.getElementById('video');
+
+
+function StartVideo() {
+    navigator.getUserMedia = (navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia);
+    navigator.getUserMedia(
+        { video: {} },
+        stream => video.srcObjet = stream,
+        err => console.log(err)
+    )
+}
+StartVideo();
---
+++ MVCTemplate/Scripts/js/main.js
@@ -0,0 +1,16 @@
+﻿const video = document.getElementById('video');
+
+function startVideo()
+{
+    navigator.getUserMedia = (navigator.getUserMedia ||
+     navigator.webkitGetUserMedia ||
+     navigator.mozGetUserMedia ||   
+        navigator.msGetUserMedia);
+    navigator.getUserMedia(
+        { video: {} },
+        stream => video.srcObjet = stream,
+        err => console.log(err)
+    )
+}
+
+startVideo();
